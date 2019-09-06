d721260091a518491d17a7c0a309497744d6594b
root <root@linux-vc98.suse>
2019-09-06 10:56:54 +0800

    第三方组件使用压缩包

--- install_dev.sh
+++ install_dev.sh
@@ -3,20 +3,22 @@
 SUFFIX=xrkmonitor.com
 cdir=`pwd`
 
+STEP_TOTAL=6
+
 # mysql 开发环境 check
 MYSQL_INCLUDE=`cat make_env |grep MYSQL_INCLUDE|awk '{print $3}'`
 MYSQL_LIB=`cat make_env |grep MYSQL_LIB|awk '{print $3}'`
 # check mysql 头文件路径是否 OK
 if [ ! -f ${MYSQL_INCLUDE}/mysql/mysql.h ]; then
-	echo "(1/5) not find file:${MYSQL_INCLUDE}/mysql/mysql.h, check mysql include path failed !"
+	echo "(1/$STEP_TOTAL) not find file:${MYSQL_INCLUDE}/mysql/mysql.h, check mysql include path failed !"
 	exit 1
 fi
 # check mysql 库文件路径是否 OK
 if [ ! -f ${MYSQL_LIB}/libmysqlclient.so ]; then
-	echo "(1/5) not find file:${MYSQL_LIB}/libmysqlclient.so, check mysql lib path failed !"
+	echo "(1/$STEP_TOTAL) not find file:${MYSQL_LIB}/libmysqlclient.so, check mysql lib path failed !"
 	exit 2
 fi
-echo "(1/5) mysql devel check ok"
+echo "(1/$STEP_TOTAL) mysql devel check ok"
 
 function InstallFailed()
 {
@@ -35,7 +37,7 @@ function InstallFailed()
 function InstallProtobuf()
 {
 	echo ""
-	read -p "(2/5) install protobuf(y/n)? " op
+	read -p "(2/$STEP_TOTAL) install protobuf(y/n)? " op
 	while [ $op != "Y" -a $op != "y" -a $op != "N" -a $op != "n" ]; do 
 		read -p "please input y/n: " op
 	done
@@ -63,7 +65,7 @@ function InstallProtobuf()
 function InstallFastcgiDev()
 {
 	echo ""
-	read -p "(3/5) install fastcgi devel(y/n)? " op
+	read -p "(3/$STEP_TOTAL) install fastcgi devel(y/n)? " op
 	while [ $op != "Y" -a $op != "y" -a $op != "N" -a $op != "n" ]; do 
 		read -p "please input y/n: " op
 	done
@@ -73,8 +75,8 @@ function InstallFastcgiDev()
 
 	if [ ! -f /usr/include/fastcgi/fastcgi.h -o ! -f /usr/lib64/libfcgi.a ]; then
 		echo "install fastcgi devel($SUFFIX)"
-		cd $cdir/lib/3rd_source_tar
-		tar -zxf fcgi-2.4.0.tar.gz; tar -zxf fcgi-2.4.0.tar.gz
+		cd $cdir/lib/fcgi
+		tar -zxf fcgi-2.4.0.tar.gz
 		cd fcgi-2.4.0
 		./configure --libdir=/usr/lib64 --includedir=/usr/include/fastcgi
 		make
@@ -93,7 +95,7 @@ function InstallFastcgiDev()
 function InstallCurlDev()
 {
 	echo ""
-	read -p "(4/5) install curl devel(y/n)? " op
+	read -p "(4/$STEP_TOTAL) install curl devel(y/n)? " op
 	while [ $op != "Y" -a $op != "y" -a $op != "N" -a $op != "n" ]; do 
 		read -p "please input y/n: " op
 	done
@@ -103,7 +105,9 @@ function InstallCurlDev()
 
 	if [ ! -f /usr/include/curl/curl.h ]; then
 		echo "install curl devel ($SUFFIX)"
-		cd $cdir/lib/curl-7.54.1
+		cd $cdir/lib/curl
+		tar -zxf curl-7.54.1.tar.gz
+		cd curl-7.54.1
 		./configure --libdir=/usr/lib64 --includedir=/usr/include
 		make
 		make install
@@ -126,11 +130,11 @@ function InstallMysqlwrap()
 		cd $cdir/lib/clearsilver; ./configure
 	
 		cd $cdir/lib/mysqlwrapped
-		echo "(5/5) preinstall mysqlwrapped ($SUFFIX)"
+		echo "(5/$STEP_TOTAL) preinstall mysqlwrapped ($SUFFIX)"
 		cat IError.h enum_t.h set_t.h Database.h Query.h > libmysqlwrapped.h
 		cp libmysqlwrapped.h /usr/include/
 	else
-		echo "(5/5) mysqlwrapped is already preinstall ($SUFFIX)"
+		echo "(5/$STEP_TOTAL) mysqlwrapped is already preinstall ($SUFFIX)"
 	fi
 
 	if [ ! -f /usr/include/libmysqlwrapped.h ]; then
@@ -139,8 +143,45 @@ function InstallMysqlwrap()
 	fi
 }
 
+# 编译生成 memcached 可执行文件
+function InstallMemcached()
+{
+	echo ""
+	read -p "(6/$STEP_TOTAL) make and install memcached(y/n)? " op
+	while [ $op != "Y" -a $op != "y" -a $op != "N" -a $op != "n" ]; do 
+		read -p "please input y/n: " op
+	done
+	if [ $op != "y" -a $op != "Y" ];then
+		return;
+	fi
+
+	if [ ! -f $cdir/slog_memcached/slog_memcached ]; then
+		echo "make memcached ($SUFFIX)"
+		cd $cdir/lib/memcached
+		tar -zxf libevent-2.0.21-stable.tar.gz
+		tar -zxf memcached-1.5.3.tar.gz
+		cd libevent-2.0.21-stable
+		./configure  --enable-static --disable-openssl --disable-shared --prefix=/usr/local/libevent
+		make
+		make install
+		cd ../memcached-1.5.3
+		./configure --prefix=/usr/local/memcached --with-libevent=/usr/local/libevent --disable-option-checking --disable-sasl --disable-sasl-pwdb --disable-coverage
+		make
+		make install
+		cp /usr/local/memcached/bin/memcached $cdir/slog_memcached/slog_memcached
+	else
+		echo "memcached is already install ($SUFFIX)"
+	fi
+
+	if [ ! -f $cdir/slog_memcached/slog_memcached ]; then
+		echo "not find file:$cdir/slog_memcached/slog_memcached"
+		InstallFailed "memcached"
+	fi
+}
+
 InstallProtobuf
 InstallFastcgiDev
 InstallCurlDev
 InstallMysqlwrap
+InstallMemcached
 
