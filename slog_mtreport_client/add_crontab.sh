5110c2ea8e815b1c59321e49a3c0d2ece87ac426
zifukj <root@linux-nonj.suse>
2019-09-19 17:46:50 +0800

    up crontab agent

--- slog_mtreport_client/add_crontab.sh
+++ slog_mtreport_client/add_crontab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 crontab -l > _add_mtreport_client_check_tmp 
-grep "check_slog_mtreport_client.sh" _add_mtreport_client_check_tmp > /dev/null 2>&1
+grep "check_xrkmonitor_agent_client.sh" _add_mtreport_client_check_tmp > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	rm _add_mtreport_client_check_tmp 
 	echo "already add"
@@ -9,7 +9,7 @@ if [ $? -eq 0 ]; then
 fi
 
 curpwd=`pwd`
-echo "*/1 * * * * ${curpwd}/check_slog_mtreport_client.sh > /dev/null 2>&1" >> _add_mtreport_client_check_tmp 
+echo "*/1 * * * * cd ${curpwd}; ./check_xrkmonitor_agent_client.sh> /dev/null 2>&1" >> _add_mtreport_client_check_tmp 
 
 crontab _add_mtreport_client_check_tmp
 if [ $? -eq 0 ]; then
--- slog_mtreport_client/remove_crontab.sh
+++ slog_mtreport_client/remove_crontab.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 crontab -l > _tmp 
-grep "check_slog_mtreport_client.sh" _tmp > /dev/null 2>&1
+grep "check_xrkmonitor_agent_client.sh" _tmp > /dev/null 2>&1
 if [ ! $? -eq 0 ]; then
         rm _tmp
         echo "already remove"
         exit 0
 fi
  
-awk '{ if(!match($0, "check_slog_mtreport_client")) print $0 }' _tmp > _remove_mtreport_client_check_tmp
+awk '{ if(!match($0, "check_xrkmonitor_agent_client")) print $0 }' _tmp > _remove_mtreport_client_check_tmp
 crontab _remove_mtreport_client_check_tmp
 if [ $? -eq 0 ];then
 	rm _tmp
