e19b98f4f3da4982505ac485fa3b85348714fac5
xrkmonitor <1820140912@qq.com>
2019-11-24 20:08:20 +0800

    fix bug add crontab

--- tools_sh/add_crontab.sh
+++ tools_sh/add_crontab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 crontab -l > _add_xrkmonitor_proc_check 
-grep "check_proc_monitor.sh" _add_xrkmonitor_proc_check > /dev/null 2>&1
+cat _add_xrkmonitor_proc_check |grep -v "^#" |grep "check_proc_monitor.sh" > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	rm _add_xrkmonitor_proc_check 
 	echo "already add"
