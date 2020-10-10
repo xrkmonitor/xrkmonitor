1c8c5c75fd27c57e78d33b1ea0378ffa0a475b25
xrkmonitor <1820140912@qq.com>
2020-10-10 15:43:42 +0800

    插件操作

--- html/resource/js/dmt.addplugin.js
+++ html/resource/js/dmt.addplugin.js
@@ -1,6 +1,69 @@
 // in dmt_dlg_machine_install_plugin.html use
 var g_addplugin_timerid = 0;
 
+// in dmt_dlg_machine_opr_plugin.html use
+var g_dmop_timerid = 0;
+
+// 引用 dmt_dp_add_plugin.html 中的 g_ddap_machlist
+function dmapGetMachineInfo(mach_id)
+{
+    if(typeof g_ddap_machlist == 'undefined')
+        return ' error, not find machine list ! ';
+    for(var i=0; i < g_ddap_machlist.count; i++) {
+        if(g_ddap_machlist.list[i].id == mach_id) {
+            return '机器：' + g_ddap_machlist.list[i].name + ' (IP:' + g_ddap_machlist.list[i].ip + ')';
+        }
+    }
+    return '机器ID:' + mach_id + '(未找到相关信息)';
+}
+
+function dmapGetOprText(st)
+{
+    var txt = '未知状态';
+    switch(st) {
+        case 1:
+            txt = '错误：机器未安装该插件';
+            break;
+        case 2:
+            txt = '最近5分钟内有未完成的操作, 请稍后再试';
+            break;
+        case 3:
+            txt = '操作中，请稍等 ...';
+            break;
+        case 4:
+            txt = '服务器发生错误，请稍后再试';
+            break;
+        case 5:
+            txt = '未找到操作任务';
+            break;
+        case 6:
+            txt = '操作超时(30s 没有完成)';
+            break;
+        case 7:
+            txt = 'agent 未启动，任务无法处理';
+            break;
+        case 8:
+            txt = 'agent 在机器上未找到该插件';
+            break;
+        case 12:
+            txt = '已发布任务';
+            break;
+        case 13:
+            txt = '任务已下发到机器';
+            break;
+        case 14:
+            txt = '操作失败，任务未完成';
+            break;
+        case 15:
+            txt = '操作成功，已完成';
+            break;
+        case 16:
+            txt = 'agent 响应超时';
+            break;
+    }
+    return txt;
+}
+
 function dmapAddPlugin_reload()
 {
     navTab.reload();
@@ -8,7 +71,69 @@ function dmapAddPlugin_reload()
         clearInterval(g_addplugin_timerid);
         g_addplugin_timerid = 0;
     }
+
+    if(g_dmop_timerid != 0) {
+        clearInterval(g_dmop_timerid);
+        g_dmop_timerid = 0;
+    }
     return true;
+}
+
+function ddapMachOprPlugin(cgip, machs, plugid, optype)
+{
+	var url = cgip + "mt_slog?action=ddap_multi_opr_plugin&machines=" + machs;
+    url += "&plugin=" + plugid;
+    url += "&opr_type=" + optype;
+	var op = {"mask":true,"maxable":false,"height":440,"width":600, "close":dmapAddPlugin_reload}; 
+    var tl = '';
+    switch(optype) {
+        case 1:
+            op.width = 700;
+            op.height = 460;
+            tl = '批量修改插件配置';
+            break;
+        case 2:
+            tl = '批量移除插件';
+            break;
+        case 3:
+            tl = '批量启用插件';
+            break;
+        case 4:
+            tl = '批量禁用插件';
+            break;
+    }
+	$.pdialog.open(url, "dlg_ddap_mach_opr_plugin", tl, op); 
+}
+
+
+function dmapMachOprPlugin(cgip, mach, plugs, optype)
+{
+	var url = cgip + "mt_slog?action=dmap_multi_opr_plugin&machine=" + mach;
+    url += "&plugins=" + plugs;
+    url += "&opr_type=" + optype;
+	var op = {"mask":true,"maxable":false,"height":440,"width":600, "close":dmapAddPlugin_reload}; 
+    var tl = '';
+    switch(optype) {
+        case 2:
+            tl = '批量移除插件';
+            break;
+        case 3:
+            tl = '批量启用插件';
+            break;
+        case 4:
+            tl = '批量禁用插件';
+            break;
+    }
+	$.pdialog.open(url, "dlg_dmap_mach_opr_plugin", tl, op); 
+}
+
+function dmapModMachPlugin(cgip, machine_id, plugid)
+{
+	var url = cgip + "mt_slog?action=ddap_multi_opr_plugin&machines=" + machine_id;
+    url += "&plugin=" + plugid;
+    url += "&opr_type=1";
+	var op = {"mask":true,"maxable":false,"height":460,"width":700, "close":dmapAddPlugin_reload};
+	$.pdialog.open(url, "dlg_ddap_mach_opr_plugin", '修改插件配置', op); 
 }
 
 function dmapAddPlugin(cgip, machid, plugid)
@@ -71,6 +196,12 @@ function dmtAddPluginStatusShow(step, rid, wid)
             break;
         case 27:
             $('#'+wid).html('<font color="red">部署失败：agent 下载开源版配置文件失败');
+            break;
+        case 28:
+            $('#'+wid).html('<font color="red">部署失败：专有配置文件下载失败');
+            break;
+        case 29:
+            $('#'+wid).html('<font color="red">部署失败：agent 响应超时');
             break;
 
         default:
